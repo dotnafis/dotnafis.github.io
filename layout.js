cbac12ba452d6a2114fc25c4cbc1ed27dbac79b0
Nafis Mahmud <101935349+dotnafis@users.noreply.github.com>
2024-01-15 01:12:52 +0600

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,10 +1,45 @@
-ellipsis={'value':['&nbsp;','.','..','...'],'count':0,'run':false,'timer':null,'element':'.loading','start':function(){var t=this;this.run=true;this.timer=setInterval(function(){if(t.run){$(t.element).html(t.value[t.count%t.value.length]).text();t.count++;}},250);},'stop':function(){this.run=false;clearInterval(this.timer);this.count=0;}}
-ellipsis.start();$(document).ready(function(){$('.indentation-container').on('click','.indentation.indentation-toggle',function(){$(this).closest('.indentation-container').addClass('collapsed');});$('.indentation-container').on('click','.indentation-ellipsis.indentation-toggle',function(){$(this).closest('.indentation-container').removeClass('collapsed');});});
+ellipsis= {
+    'value': ['&nbsp;', '.', '..', '...'], 'count':0, 'run':false, 'timer':null, 'element':'.loading', 'start':function() {
+        var t=this;
+        this.run=true;
+
+        this.timer=setInterval(function() {
+                if(t.run) {
+                    $(t.element).html(t.value[t.count%t.value.length]).text(); t.count++;
+                }
+            }
+
+            , 250);
+    }
+
+    ,
+    'stop':function() {
+        this.run=false;
+        clearInterval(this.timer);
+        this.count=0;
+    }
+}
+
+ellipsis.start();
+
+$(document).ready(function() {
+        $('.indentation-container').on('click', '.indentation.indentation-toggle', function() {
+                $(this).closest('.indentation-container').addClass('collapsed');
+            }
+
+        ); $('.indentation-container').on('click', '.indentation-ellipsis.indentation-toggle', function() {
+                $(this).closest('.indentation-container').removeClass('collapsed');
+            }
+
+        );
+    }
+
+);
 
 
 /** Custom Cursor */
 const cursor = document.querySelector(".cursor");
-let anchors  = document.querySelectorAll("a");
+let anchors = document.querySelectorAll("a");
 
 document.addEventListener("mousemove", (e) => {
 	cursor.style.top = e.pageY + "px";
